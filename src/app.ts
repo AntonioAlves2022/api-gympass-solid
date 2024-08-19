955eb9d31d22b16332f40b504b138ac107dd54a6
Antonio Alves <antonio.alves@duque.g12.br>
2024-08-19 19:33:01 -0300

    Criando os controllers da aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 import {z} from 'zod'
-import { prisma } from "./libs/prisma";
+import { hash } from 'bcryptjs'
+import { prisma } from './libs/prisma'
 
 export const app = fastify();
 
 app.post('/users', async(request, reply)=>{
-  
+  const userBodySchema = z.object({
+    name: z.string().min(3).max(100),
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
+
+  const {name, email, password} = userBodySchema.parse(request.body)
+  const password_hash = await hash(password, 6)
+  await prisma.user.create({
+    data:{
+      name,
+      email,
+      password_hash
+    }
+  })
+
+  return reply.status(201).send()
 })
