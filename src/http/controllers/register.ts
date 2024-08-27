3e882319ffb0245005e7936a7d1e3328af123111
Antonio Alves <antonio.alves@duque.g12.br>
2024-08-26 21:47:34 -0300

    Escrevendo o primeiro teste automatizado

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -20,7 +20,8 @@ export async function register(request:FastifyRequest, reply:FastifyReply){
     if(error instanceof UserAlreadyExistsError){
       return reply.status(409).send({message:error.message})
     }
-    return reply.status(500).send()
+    // Lançar o erro para a próxima camada tratar (app.ts)
+    throw error
 
   }
   return reply.status(201).send()
---
+++ src/usecases/register.spec.ts
@@ -0,0 +1,4 @@
+import {test, expect} from 'vitest'
+test('1+1 = 2',()=>{
+  expect(1+1).toBe(2)
+})
