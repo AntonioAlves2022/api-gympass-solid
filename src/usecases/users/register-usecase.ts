d81a25ec20b8ee73d95a509674d88b7218f79f6d
Antonio Alves <antonio.alves@duque.g12.br>
2024-09-04 14:33:58 -0300

    Aula sobre testes unitários realizada no dia 02/09/2024

--- src/usecases/users/register-usecase.ts
+++ src/usecases/users/register-usecase.ts
@@ -1,16 +1,22 @@
 import {hash} from 'bcryptjs'
 import type { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from '../errors/user-already-exists'
+import type { User } from '@prisma/client'
+
 interface RegisterUseCaseRequest{
   name:string
   email:string
   password:string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUserUseCase{
   constructor(private usersRepository:UsersRepository){}
 
-  async execute({name, email, password}: RegisterUseCaseRequest){
+  async execute({name, email, password}: RegisterUseCaseRequest):Promise<RegisterUseCaseResponse> {
     // Criptografar a senha do usuario
     const password_hash = await hash(password, 6)
     // Verificar se o e-mail de registro já está em uso
@@ -22,6 +28,8 @@ export class RegisterUserUseCase{
     const user = await this.usersRepository.create({
       name, email, password_hash
     })
+
+    return {user}
   }
 }
 
