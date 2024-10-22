cd9345c664b67e8a3ec7dd948552f88ba2783937
Antonio Alves <antonio.alves@duque.g12.br>
2024-10-21 21:08:57 -0300

    Aula do dia 21/10/2024 - Caso de uso do CheckIn

--- src/usecases/get-user-profile-usecase.spec.ts
+++ src/usecases/get-user-profile-usecase.spec.ts
@@ -1,6 +1,7 @@
 import { UsersInMemoryRepository } from "@/repositories/users-in-memory-repository";
 import { describe, it, expect, beforeEach } from "vitest";
 import { GetUserProfileUseCase } from "./users/get-user-profile";
+import { hash } from "bcryptjs";
 let usersRepository:UsersInMemoryRepository
 let sut:GetUserProfileUseCase
 describe('Get user profile use case', ()=>{
@@ -9,7 +10,17 @@ describe('Get user profile use case', ()=>{
     sut = new GetUserProfileUseCase(usersRepository)
   })
   //1 º Teste
-  it('It should be able to get user profile by id', async()=>{
+  it('It should be able to get user profile', async()=>{
+    const newUser = await usersRepository.create({
+      name: 'Fulano de Tal',
+      email:'fulaninho@google.org.br',
+      password_hash: await hash('123456', 6)
+    })
 
+    const { user } = await sut.execute({
+      userId: newUser.id
+    })
+
+    expect(user.id).toEqual(expect.any(String))
   })
 })
