894256a5b44bdf17334ff330314496811cd3068e
Antonio Alves <antonio.alves@duque.g12.br>
2024-11-04 18:41:53 -0300

    Aula do dia 04/11/2024

---
+++ src/repositories/GymsRepository.ts
@@ -0,0 +1,5 @@
+import type { Gym } from "@prisma/client";
+
+export interface GymsRepository{
+  findById(id:String):Promise<Gym|null>
+}
--- src/usecases/checkins/check-in-usecase.ts
+++ src/usecases/checkins/check-in-usecase.ts
@@ -1,9 +1,13 @@
 import type { CheckInsRepository } from '@/repositories/check-ins-repository'
+import type { GymsRepository } from '@/repositories/GymsRepository'
 import {CheckIn} from '@prisma/client'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
 
 export interface CheckInUseCaseRequest{
   userId: string
   gymId: string
+  userLatitude:number
+  userLongitude:number
 }
 
 export interface CheckInUseCaseResponse{
@@ -11,8 +15,17 @@ export interface CheckInUseCaseResponse{
 }
 
 export class CheckInUseCase{
-  constructor(private checkInsRepository: CheckInsRepository){}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository:GymsRepository
+  ){}
   async execute({userId, gymId}:CheckInUseCaseRequest): Promise<CheckInUseCaseResponse>{
+    const gym = await this.gymsRepository.findById(gymId)
+    if(!gym){
+      throw new ResourceNotFoundError()
+    }
+
+    // vamos calcular a distancia que o user está da academia
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
   if(checkInOnSameDay){
     throw new Error()
